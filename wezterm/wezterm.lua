7d3002b99391329be655e56001399c751a6f00aa
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2024-10-03 12:06:52 -0300

    update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local config = {}
 
 config.color_scheme = 'Dracula (Official)'
 config.font = wezterm.font 'Roboto Mono'
-config.font_size = 15.0
+config.font_size = 16.0
 
 config.send_composed_key_when_left_alt_is_pressed = true
 config.show_new_tab_button_in_tab_bar = false
@@ -11,8 +11,8 @@ config.show_tabs_in_tab_bar = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.75
 config.macos_window_background_blur = 30
-config.window_decorations = "RESIZE"
+config.window_decorations = "TITLE | RESIZE | MACOS_FORCE_ENABLE_SHADOW"
 
 return config
