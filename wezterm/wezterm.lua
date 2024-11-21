d53b8d05d50d964e613485cb77ff1102a1dd1948
Leonardo Veiga <lveiga.jp@gmail.com>
2024-11-21 11:36:07 -0300

    changing some wezterm gui config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,8 +1,8 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = 'Dracula (Official)'
-config.font = wezterm.font 'Roboto Mono'
+config.color_scheme = "Dracula (Official)"
+config.font = wezterm.font("Roboto Mono")
 config.font_size = 16.0
 
 config.send_composed_key_when_left_alt_is_pressed = true
@@ -11,23 +11,23 @@ config.show_tabs_in_tab_bar = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 
-config.window_background_opacity = 0.75
-config.macos_window_background_blur = 30
+config.window_background_opacity = 0.90
+config.macos_window_background_blur = 100
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE | MACOS_FORCE_ENABLE_SHADOW"
 
 config.window_padding = {
-  left = 15,
-  right = 15,
-  top = 55,
-  bottom = 0,
+	left = 20,
+	right = 20,
+	top = 55,
+	bottom = 0,
 }
 
 config.keys = {
-  {
-    key = 'w',
-    mods = 'CMD',
-    action = wezterm.action.CloseCurrentTab { confirm = false },
-  },
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentTab({ confirm = false }),
+	},
 }
 
 return config
